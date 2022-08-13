d0ecda0a877cc262c3030fac21545fd9b2377cd9
E.B. Plat <evan.plat@hva.nl>
2022-08-13 10:00:23 +0200

    Updated example

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -38,6 +38,12 @@ foreach (DeviceClient deviceClient in deviceClients)
         await acClient.SetSnapshotValue(snapshot => snapshot.IsOn, true);
         // or
         await acClient.SetOnState(true);
+        // or send multiple values
+        await acClient.SendMultipleValues(TimeSpan.FromMilliseconds(100))
+            .Value(s => s.IsOn, true)
+            .Value(s => s.OperationMode, (double)AcMode.Cool)
+            .Value(s => s.FanSpeed, (double)AcFanSpeed.High)
+            .SendCommandsAsync();
     }
     deviceClient.OnDeviceSnapshotChanged += (sender, args) =>
     {
